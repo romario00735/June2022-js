53bd553a348e620e969bcc7c26fdeb9b29f6917a
romario00735 <romario977@ukr.net>
2022-07-22 17:47:39 +0300

    JS

---
+++ lesson1/hw1/main.js
@@ -0,0 +1,105 @@
+// let str1 = 'hello';
+// console.log(str1);
+// document.write(str1);
+// alert(str1);
+//
+// let str2 = 'owu';
+// console.log(str2);
+// alert(str2);
+// document.write(str2);
+//
+// let str3 = 'com';
+// console.log(str3);
+// alert(str3);
+// document.write(str3);
+//
+// let str4 = 'ua';
+// console.log(str4);
+// alert(str4);
+// document.write(str4);
+//
+// let a = 1;
+// console.log(a);
+// alert(a);
+// document.write(a);
+//
+// let b = 10;
+// console.log(b);
+// alert(b);
+// document.write(b);
+//
+// let c = -999;
+// console.log(c);
+// alert(c);
+// document.write(c);
+//
+// let d = 123;
+// console.log(d);
+// alert(d);
+// document.write(d);
+//
+// let e = 3.14;
+// console.log(e);
+// alert(e);
+// document.write(e);
+//
+// let f = 2.7;
+// console.log(f);
+// alert(f);
+// document.write(f);
+//
+// let g = 16;
+// console.log(g);
+// alert(g);
+// document.write(g);
+//
+// let bool1 = true;
+// console.log(bool1);
+// alert(bool1);
+// document.write(bool1);
+//
+// let bool2 = false;
+// console.log(bool2);
+// alert(bool2);
+// document.write(bool2);
+
+
+
+// let firstName = 'Роман';
+// console.log(firstName);
+//
+//
+// let middleName = 'Ігорович';
+// console.log(middleName);
+//
+// let lastName = 'Перегонцев';
+// console.log(lastName);
+//
+// let person = lastName + ' ' + firstName +' ' +middleName;
+// console.log(person);
+// document.write(person);
+
+
+
+// let a = 100;
+// console.log(typeof a);
+//
+// let b = '100';
+// console.log(typeof b);
+//
+// let c = true;
+// console.log(typeof c);
+
+
+
+let name = 'Роман';
+prompt(name);
+console.log(name);
+
+let fname = 'Ігорович'
+prompt(fname);
+console.log(fname);
+
+let age = '27';
+prompt(age);
+console.log(age);
