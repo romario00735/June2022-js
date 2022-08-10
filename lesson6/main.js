5db3c91c29d05877bd4ae4a92a014008a60ff3c8
romario00735 <romario977@ukr.net>
2022-08-10 18:14:48 +0300

    6

--- lesson6/main.js
+++ lesson6/main.js
@@ -19,73 +19,143 @@
 // console.log(str1.toLowerCase(), str2.toLowerCase(), str3.toLowerCase());
 
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
-let str1 = ' dirty string   '
-console.log(str1.replaceAll(' ', ''));
+// let str1 = ' dirty string   '
+// console.log(str1.replaceAll(' ', ''));
 
 
 
  // - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
 //     let str = 'Ревуть воли як ясла повні';
-let str = 'Ревуть воли як ясла повні';
-let split = str.split(' ');
-console.log(split);
-
-
+// let str = 'Ревуть воли як ясла повні';
+// let split = str.split(' ');
+// console.log(split);
+//
+//
 
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
 // let arr = [10, 8, -7, 55, 987, -1011, 0, 1050, 0];
 // let str = arr.map(String);
 // console.log(str);
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
-    let nums = [11,21,3];
-sortNums(nums,'ascending') // [3,11,21]
-sortNums(nums,'descending') // [21,11,3]
-let nums = [11,21,3];
-function sortNums(number,direction){
-    if (direction === 'ascending') {
-        number.sort((num1, num2) => num1 - num2)
-    }
-    else if (direction === 'descending') {
-        number.sort((num1, num2) => num2 - num1);
-
-    }
-}
-sortNums(nums, 'ascending');
-console.log(nums);
-sortNums(nums, 'descending');
-console.log(nums);
+//     let nums = [11,21,3];
+// sortNums(nums,'ascending') // [3,11,21]
+// sortNums(nums,'descending') // [21,11,3]
+// let nums = [11,21,3];
+// function sortNums(number,direction){
+//     if (direction === 'ascending') {
+//         number.sort((num1, num2) => num1 - num2)
+//     }
+//     else if (direction === 'descending') {
+//         number.sort((num1, num2) => num2 - num1);
+//
+//     }
+// }
+// sortNums(nums, 'ascending');
+// console.log(nums);
+// sortNums(nums, 'descending');
+// console.log(nums);
 
 
 // -- відсортувати його за спаданням за monthDuration
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
-let coursesAndDurationArray = [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
-];
-let sort = coursesAndDurationArray.sort((s1,s2)=>{
-    return s1.monthDuration - s2.monthDuration;
-})
-console.log(sort);
-coursesAndDurationArray.forEach ((monthD) =>{
-    if (monthD.monthDuration > 5) {
-        console.log(monthD);
-    }
-})
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// let sort = coursesAndDurationArray.sort((s1,s2)=>{
+//     return s1.monthDuration - s2.monthDuration;
+// })
+// console.log(sort);
+// coursesAndDurationArray.forEach((monthD) => {
+//     if (monthD.monthDuration > 5) {
+//         console.log(monthD);
+//     }
+// });
 
 
 
 
 
 // описати колоду карт
+
+let arrCards = [
+    {cardSuit: 'clubs', value: 'Ace', color:'black'},
+    {cardSuit: 'clubs', value: 2, color:'black'},
+    {cardSuit: 'clubs', value: 3, color:'black'},
+    {cardSuit: 'clubs', value: 4, color:'black'},
+    {cardSuit: 'clubs', value: 5, color:'black'},
+    {cardSuit: 'clubs', value: 6, color:'black'},
+    {cardSuit: 'clubs', value: 7, color:'black'},
+    {cardSuit: 'clubs', value: 8, color:'black'},
+    {cardSuit: 'clubs', value: 9, color:'black'},
+    {cardSuit: 'clubs', value: 10, color:'black'},
+    {cardSuit: 'clubs', value: 'Jack', color:'black'},
+    {cardSuit: 'clubs', value: 'Queen', color:'black'},
+    {cardSuit: 'clubs', value: 'King', color:'black'},
+
+    {cardSuit: 'diamonds', value: 'Ace', color:'red'},
+    {cardSuit: 'diamonds', value: 2, color:'red'},
+    {cardSuit: 'diamonds', value: 3, color:'red'},
+    {cardSuit: 'diamonds', value: 4, color:'red'},
+    {cardSuit: 'diamonds', value: 5, color:'red'},
+    {cardSuit: 'diamonds', value: 6, color:'red'},
+    {cardSuit: 'diamonds', value: 7, color:'red'},
+    {cardSuit: 'diamonds', value: 8, color:'red'},
+    {cardSuit: 'diamonds', value: 9, color:'red'},
+    {cardSuit: 'diamonds', value: 10, color:'red'},
+    {cardSuit: 'diamonds', value: 'Jack', color:'red'},
+    {cardSuit: 'diamonds', value: 'Queen', color:'red'},
+    {cardSuit: 'diamonds', value: 'King', color:'red'},
+
+    {cardSuit: 'hearts', value: 'Ace', color:'red'},
+    {cardSuit: 'hearts', value: 2, color:'red'},
+    {cardSuit: 'hearts', value: 3, color:'red'},
+    {cardSuit: 'hearts', value: 4, color:'red'},
+    {cardSuit: 'hearts', value: 5, color:'red'},
+    {cardSuit: 'hearts', value: 6, color:'red'},
+    {cardSuit: 'hearts', value: 7, color:'red'},
+    {cardSuit: 'hearts', value: 8, color:'red'},
+    {cardSuit: 'hearts', value: 9, color:'red'},
+    {cardSuit: 'hearts', value: 10, color:'red'},
+    {cardSuit: 'hearts', value: 'Jack', color:'red'},
+    {cardSuit: 'hearts', value: 'Queen', color:'red'},
+    {cardSuit: 'hearts', value: 'King', color:'red'},
+    {value: 'Joker', color:'red'},
+
+    {cardSuit: 'spades', value: 'Ace', color:'black'},
+    {cardSuit: 'spades', value: 2, color:'black'},
+    {cardSuit: 'spades', value: 3, color:'black'},
+    {cardSuit: 'spades', value: 4, color:'black'},
+    {cardSuit: 'spades', value: 5, color:'black'},
+    {cardSuit: 'spades', value: 6, color:'black'},
+    {cardSuit: 'spades', value: 7, color:'black'},
+    {cardSuit: 'spades', value: 8, color:'black'},
+    {cardSuit: 'spades', value: 9, color:'black'},
+    {cardSuit: 'spades', value: 10, color:'black'},
+    {cardSuit: 'spades', value: 'Jack', color:'black'},
+    {cardSuit: 'spades', value: 'Queen', color:'black'},
+    {cardSuit: 'spades', value: 'King', color:'black'},
+    {value: 'Joker', color:'black'},
+
+];
 // - знайти піковий туз
+console.log(arrCards.filter(value => value.value === 'Ace' && value.cardSuit === 'spades'));
 // - всі шістки
+let find = arrCards.filter(value => value.value === 6)
+console.log(find);
 // - всі червоні карти
+console.log(arrCards.filter(value => value.color === 'red'));
+
 // - всі буби
+console.log(arrCards.filter(value => value.cardSuit === 'diamonds'));
+
 // - всі трефи від 9 та більше
+console.log(arrCards.filter(value => value.value >= 9 && value.cardSuit === 'clubs'));
+
 //
 //
 // {
